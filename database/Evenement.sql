a66e23473f66f5f2bedd3c78be7338268da51227
SoleilDivin <vinesh.sinel@gmail.com>
2023-06-19 10:04:00 +0300

    Add Material List and AddForm

--- database/Evenement.sql
+++ database/Evenement.sql
@@ -4,9 +4,9 @@ ALTER DATABASE evento OWNER TO evento;
 
 CREATE TABLE "token"
 (
-    "id"      serial PRIMARY KEY,
-    "user_id" integer      NOT NULL,
-    "token"   varchar(255) NOT NULL
+    "id"          serial PRIMARY KEY,
+    "app_user_id" integer      NOT NULL,
+    "token"       varchar(255) NOT NULL
 );
 
 CREATE TABLE "user_type"
@@ -15,7 +15,7 @@ CREATE TABLE "user_type"
     "name" varchar(255) NOT NULL
 );
 
-CREATE TABLE "user"
+CREATE TABLE "app_user"
 (
     "id"           serial PRIMARY KEY,
     "username"     varchar(255) NOT NULL,
@@ -30,22 +30,56 @@ CREATE TABLE "event_type"
     "name" varchar(255) NOT NULL
 );
 
+CREATE TABLE "location_type"
+(
+    "id"   serial PRIMARY KEY,
+    "name" varchar(255) NOT NULL
+);
+
 CREATE TABLE "location"
 (
-    "id"           serial PRIMARY KEY,
-    "name"         varchar(255)   NOT NULL,
-    "max_capacity" integer        NOT NULL,
-    "rent_price"   numeric(15, 2) NOT NULL
+    "id"            serial PRIMARY KEY,
+    "name"          varchar(255)   NOT NULL,
+    "max_capacity"  integer        NOT NULL,
+    "location_type" integer,
+    "rent_price"    numeric(15, 2) NOT NULL
+);
+
+CREATE TABLE "event_status"
+(
+    "id"   serial PRIMARY KEY,
+    "name" varchar(255) NOT NULL
 );
 
 CREATE TABLE "event"
 (
+    "id"                         serial PRIMARY KEY,
+    "name"                       varchar(255) NOT NULL,
+    "event_type_id"              integer      NOT NULL,
+    "location_id"                integer      NOT NULL,
+    "sound_system_id"            integer      NOT NULL,
+    "sound_system_hour_duration" integer      NOT NULL,
+    "logistics_id"               integer      NOT NULL,
+    "start_date"                 timestamp    NOT NULL,
+    "end_date"                   timestamp    NOT NULL,
+    "tour_id"                    integer,
+    "deleted"                    boolean      NOT NULL,
+    "event_status_id"            integer      NOT NULL
+);
+
+CREATE TABLE "event_artist"
+(
     "id"            serial PRIMARY KEY,
-    "name"          varchar(255) NOT NULL,
-    "event_type_id" integer      NOT NULL,
-    "location_id"   integer      NOT NULL,
-    "date"          timestamp    NOT NULL,
-    "tour_id"       integer
+    "event_id"      integer NOT NULL,
+    "artist_id"     integer NOT NULL,
+    "hour_duration" integer NOT NULL
+);
+
+CREATE TABLE "event_expenses"
+(
+    "id"              serial PRIMARY KEY,
+    "expense_type_id" integer        NOT NULL,
+    "amount"          numeric(15, 2) NOT NULL
 );
 
 CREATE TABLE "event_seat_category"
@@ -88,8 +122,41 @@ CREATE TABLE "tour"
     "end_date"   date         NOT NULL
 );
 
-ALTER TABLE app_user
+CREATE TABLE "artist"
+(
+    "id"          serial PRIMARY KEY,
+    "name"        varchar(255)   NOT NULL,
+    "hourly_rate" numeric(15, 2) NOT NULL
+);
+
+CREATE TABLE "sound_system"
+(
+    "id"          serial PRIMARY KEY,
+    "name"        varchar(255)   NOT NULL,
+    "hourly_rate" numeric(15, 2) NOT NULL
+);
+
+CREATE TABLE "logistics"
+(
+    "id"         serial PRIMARY KEY,
+    "name"       varchar(255)   NOT NULL,
+    "daily_rate" numeric(15, 2) NOT NULL
+);
+
+CREATE TABLE "expense_type"
+(
+    "id"   serial PRIMARY KEY,
+    "name" varchar(255) NOT NULL
+);
+
+ALTER TABLE "token"
+    ADD FOREIGN KEY ("app_user_id") REFERENCES "app_user" ("id");
+
+ALTER TABLE "app_user"
     ADD FOREIGN KEY ("user_type_id") REFERENCES "user_type" ("id");
+
+ALTER TABLE "location"
+    ADD FOREIGN KEY ("location_type") REFERENCES "location_type" ("id");
 
 ALTER TABLE "event"
     ADD FOREIGN KEY ("event_type_id") REFERENCES "event_type" ("id");
@@ -98,7 +165,25 @@ ALTER TABLE "event"
     ADD FOREIGN KEY ("location_id") REFERENCES "location" ("id");
 
 ALTER TABLE "event"
+    ADD FOREIGN KEY ("sound_system_id") REFERENCES "sound_system" ("id");
+
+ALTER TABLE "event"
+    ADD FOREIGN KEY ("logistics_id") REFERENCES "logistics" ("id");
+
+ALTER TABLE "event"
     ADD FOREIGN KEY ("tour_id") REFERENCES "tour" ("id");
+
+ALTER TABLE "event"
+    ADD FOREIGN KEY ("event_status_id") REFERENCES "event_status" ("id");
+
+ALTER TABLE "event_artist"
+    ADD FOREIGN KEY ("event_id") REFERENCES "event" ("id");
+
+ALTER TABLE "event_artist"
+    ADD FOREIGN KEY ("artist_id") REFERENCES "artist" ("id");
+
+ALTER TABLE "event_expenses"
+    ADD FOREIGN KEY ("expense_type_id") REFERENCES "expense_type" ("id");
 
 ALTER TABLE "event_seat_category"
     ADD FOREIGN KEY ("event_id") REFERENCES "event" ("id");
@@ -111,5 +196,3 @@ ALTER TABLE "event_material"
 
 ALTER TABLE "ticket_sale"
     ADD FOREIGN KEY ("event_seat_category_id") REFERENCES "event_seat_category" ("id");
-ALTER TABLE "token"
-    ADD FOREIGN KEY (app_user_id) REFERENCES app_user ("id");
